565bfbea6d604d52240773a59ec21e7aec2833f0
NICK SEBASTIAN VALVERDE ZAVALA <nick.valverde@epn.edu.ec>
2024-07-10 20:03:10 -0500

    Carga el homero y movi un pcocoel fragmetn y cargan las imagenes

---
+++ OpenGLNV/shaders/shader_B2T2.fs
@@ -0,0 +1,32 @@
+#version 330 core
+out vec4 FragColor;
+
+in vec2 TexCoord;
+
+// texture samplers
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+uniform sampler2D texture3;
+
+uniform int variable;
+
+void main()
+{
+
+    if(varriable == 1){
+        FragColor = texture(texture1, TexCoord);
+    }
+    else if(varriable == 2){
+        FragColor = texture(texture2, TexCoord);
+    }
+    else if(varriable == 3){
+        FragColor = texture(texture3, TexCoord);
+    }
+    else{
+        FragColor = texture(texture1, TexCoord);
+    }
+
+// linearly interpolate between both textures (80% container, 20% awesomeface)
+//FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
+//FragColor = texture(texture1, TexCoord);
+}
